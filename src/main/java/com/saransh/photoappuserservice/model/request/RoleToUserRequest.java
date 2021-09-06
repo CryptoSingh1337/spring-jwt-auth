a4c39fe47f84801a9e4c9537cedc8e2588f8eb38
Saransh Kumar <cryptosingh@gmail.com>
2021-09-06 22:15:00 +0530

    Created request model for adding Role to a User.

---
+++ src/main/java/com/saransh/photoappuserservice/model/request/RoleToUserRequest.java
@@ -0,0 +1,12 @@
+package com.saransh.photoappuserservice.model.request;
+
+import lombok.Data;
+
+/**
+ * Created by CryptSingh1337 on 9/6/2021
+ */
+@Data
+public class RoleToUserRequest {
+    private String username;
+    private String role;
+}
